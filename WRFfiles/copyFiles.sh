0a28191e4360f491f162d8943e999931322a2637
sviaroCryos <samuele.viaro@epfl.ch>
2026-03-11 18:37:42 +0100

    update bash script to copy files

--- WRFfiles/copyFiles.sh
+++ WRFfiles/copyFiles.sh
@@ -8,12 +8,14 @@
 DEST1=/home/viaro/cryos/softwares/cryowrf/CRYOWRF-WRF/test/em_real/input
 DEST2=/home/viaro/cryos/softwares/cryowrf/CRYOWRF-WRF/test/em_real
 DEST3=/home/viaro/cryos/softwares/cryowrf/CRYOWRF-WRF/phys
+DEST4=/home/viaro/cryos/softwares/cryowrf/CRYOWRF-WRF/test/em_real/wpsout
 
 
-#copying
+#copy DEST1
 cp COSMO.smet $DEST1
 cp make_sno_for_sam.py $DEST1
 
+#copy DEST2
 cp io.ini $DEST2
 cp namelist.input $DEST2
 cp outhist_variables_ALPS.txt $DEST2
@@ -21,7 +23,22 @@ cp tslist $DEST2
 cp WRF.job $DEST2
 cp WRF_REAL.job $DEST2
 
+#copy DEST3: compare this file with the local first
 cp module_sf_snowpacklsm_TESTCASE.F $DEST3
+
+#copy DEST4
+DIR=$DEST4
+if [ -d "$DIR" ]; then
+    # If it exists, remove everything inside but keep the folder
+    rm -rf "${DIR:?}"/*
+    echo "Directory '$DIR' cleaned."
+else
+    # If it doesn't exist, create it
+    mkdir "$DIR"
+    echo "Directory '$DIR' created."
+fi
+cp metFiles/met_em* $DEST4
+
 
 
 
