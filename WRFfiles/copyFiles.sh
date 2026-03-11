febc3d1da77b7d1e080efcfdf4d3bc55a111cb87
sviaroCryos <samuele.viaro@epfl.ch>
2026-03-11 09:27:16 +0100

    bash to copy WRF files

---
+++ WRFfiles/copyFiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#@sviaro
+#This script copies and overwrites the necessary files to run CRYOWRF for the testcase
+#
+
+#variables
+DEST1=/home/viaro/cryos/softwares/cryowrf/CRYOWRF-WRF/test/em_real/input
+DEST2=/home/viaro/cryos/softwares/cryowrf/CRYOWRF-WRF/test/em_real
+DEST3=/home/viaro/cryos/softwares/cryowrf/CRYOWRF-WRF/phys
+
+
+#copying
+cp COSMO.smet $DEST1
+cp make_sno_for_sam.py $DEST1
+
+cp io.ini $DEST2
+cp namelist.input $DEST2
+cp outhist_variables_ALPS.txt $DEST2
+cp tslist $DEST2
+cp WRF.job $DEST2
+cp WRF_REAL.job $DEST2
+
+cp module_sf_snowpacklsm_TESTCASE.F $DEST3
+
+
+
+
+
+
+
